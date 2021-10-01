8de021f3df86aa28fa333e21f506c02e3d9fb88a
Christopher Brown <pyrrhicpachyderm@gmail.com>
2021-10-02 01:02:46 +1300

    Add ParameterVector and Jacobian typedefs to the Solver class.

--- src/solver.hpp
+++ src/solver.hpp
@@ -18,6 +18,17 @@ class Solver {
 			assert(rowGrouping.numSpecies == data.numSpecies);
 			assert(colGrouping.numSpecies == data.numSpecies);
 		}
+		
+		//To ease matrix manipulation, the set of parameters will just be represented as a vector.
+		//In reality, it will consist of:
+		//one growth rate for each growth rate group, then
+		//one competition coefficient for each pair of a row group and a column group, in row-major order.
+		typedef Eigen::VectorXd ParameterVector;
+		
+		//Similarly, the Jacobian will be represented simply as a matrix.
+		//It has a number of rows equal to the number of observations, and
+		//a number of columns equal to the number of parameters, as given above.
+		typedef Eigen::MatrixXd Jacobian;
 };
 
 #endif
