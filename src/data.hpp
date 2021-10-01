afc23f06f40cc15d3303dd2a78b36ccba2ac0b70
Christopher Brown <pyrrhicpachyderm@gmail.com>
2021-10-01 22:54:31 +1300

    Add the Data class to the C++ code.

---
+++ src/data.hpp
@@ -0,0 +1,30 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+
+#include <assert.h>
+#include <vector>
+#include <Eigen/Core>
+
+class Data {
+	public:
+		const size_t numSpecies;
+		const size_t numObservations;
+	protected:
+		//The index of the species used as the focal in each observation.
+		std::vector<size_t> focal;
+		
+		//The vector of response variables, one for each observation.
+		Eigen::VectorXd response;
+		
+		//The design density matrix, with numSpecies columns and numObservations rows.
+		Eigen::MatrixXd design;
+	public:
+		Data(std::vector<size_t> focal, Eigen::VectorXd response, Eigen::MatrixXd design):
+			numSpecies(design.cols()), numObservations(design.rows()), focal(focal), response(response), design(design)
+		{
+			assert(focal.size() == numObservations);
+			assert((size_t)response.rows() == numObservations);
+		};
+};
+
+#endif
