678add398ee9b6691431dba3a919e0a6e5894b8c
Christopher Brown <pyrrhicpachyderm@gmail.com>
2021-10-05 20:44:00 +1300

    Add R squared values to the brute force output table.

--- src/brute.cpp
+++ src/brute.cpp
@@ -62,16 +62,18 @@ int main(int argc, char **argv) {
 	OutputColumn<Grouping> outputRowGroupings("row_group");
 	OutputColumn<Grouping> outputColGroupings("col_group");
 	OutputColumn<double> outputAICs("aic");
+	OutputColumn<double> outputR2s("R2");
 	
 	do {
 		do {
 			outputRowGroupings.insert(solver.getRowGrouping());
 			outputColGroupings.insert(solver.getColGrouping());
 			outputAICs.insert(solver.getAIC());
+			outputR2s.insert(solver.getR2());
 		} while(solver.updateRowGrouping(&Grouping::advance));
 	} while(solver.updateColGrouping(&Grouping::advance));
 	
-	outputTable(outputFile, outputRowGroupings, outputColGroupings, outputAICs);
+	outputTable(outputFile, outputRowGroupings, outputColGroupings, outputAICs, outputR2s);
 	
 	return 0;
 }
