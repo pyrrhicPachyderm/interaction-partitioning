a9929e3f76ac60425f477861c64ad92698efd5db
Christopher Brown <pyrrhicpachyderm@gmail.com>
2021-10-05 22:46:53 +1300

    Rewrite the brute force post processing code using R6.

--- r/brute-post-process.R
+++ r/brute-post-process.R
@@ -1,26 +1,43 @@
 #A set of helper functions for post-processing the data from brute forcing.
-#Does not read in any data; data must all be passed in by arguments.
-#Data is assumed to be passed as produced by the C++ program, however, with column names preserved.
-#Additionally, some of the functions rely on a vector of species names.
+#Works in an object-oriented fashion.
+#Constructing the central object requires a data file produced by the C++ program, with column names preserved.
+#Additionally, it requires a vector of species names.
 #These must be provided in the order that the C++ program indexed the species.
 
-validate_data <- function(data, species_names) {
-	#Ensures that this is data for the correct number of species.
-	if(
-		length(grep("row_group", names(data))) != length(species_names) |
-		length(grep("col_group", names(data))) != length(species_names)
-	) {
-		stop("Data frame has the wrong number of species.")
-	}
-}
-
-get_num_species <- function(data) {
-	length(grep("row_group", names(data)))
-}
-
-get_grouping <- function(data, index, grouping_prefix) {
-	as.vector(as.matrix(data[index, grep(grouping_prefix, names(data))]))
-}
-
-get_row_grouping <- function(data, index) {get_grouping(data, index, "row_group")}
-get_col_grouping <- function(data, index) {get_grouping(data, index, "col_group")}
+BruteData <- R6::R6Class("BruteData",
+	public = list(
+		num_species = NULL,
+		species_names = NULL,
+		row_groupings = NULL, #A data frame of row groupings, named by the species names.
+		col_groupings = NULL, #As above, for col groupings.
+		statistics = NULL, #A data frame of all output values besides groupings.
+		
+		initialize = function(data_file_name, species_names) {
+			data_table <- read.table(data_file_name, header=TRUE)
+			
+			self$num_species <- length(species_names)
+			self$species_names <- species_names
+			
+			private$validate_data(data_table, self$num_species)
+			
+			#Extract the groupings and other data.
+			self$row_groupings <- data_table[,grep("row_group", names(data_table))]
+			self$col_groupings <- data_table[,grep("col_group", names(data_table))]
+			names(self$row_groupings) <- species_names
+			names(self$col_groupings) <- species_names
+			self$statistics <- data_table[,grep("row_group|col_group", names(data_table), invert=TRUE)]
+		}
+	),
+	
+	private = list(
+		validate_data = function(data_table, num_species) {
+			#Checks whether a data table is for the correct number of species.
+			if(
+				length(grep("row_group", names(data_table))) != num_species |
+				length(grep("col_group", names(data_table))) != num_species
+			) {
+				stop("Data table has the wrong number of species.")
+			}
+		}
+	)
+)
