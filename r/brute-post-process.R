9bee8b77b73172a88a7511739625a9bae1236a2b
Christopher Brown <pyrrhicpachyderm@gmail.com>
2021-10-05 20:16:29 +1300

    Add a post processing function to validate the data.

--- r/brute-post-process.R
+++ r/brute-post-process.R
@@ -3,3 +3,13 @@
 #Data is assumed to be passed as produced by the C++ program, however, with column names preserved.
 #Additionally, some of the functions rely on a vector of species names.
 #These must be provided in the order that the C++ program indexed the species.
+
+validate_data <- function(data, species_names) {
+	#Ensures that this is data for the correct number of species.
+	if(
+		length(grep("row_group", names(data))) != length(species_names) |
+		length(grep("col_group", names(data))) != length(species_names)
+	) {
+		stop("Data frame has the wrong number of species.")
+	}
+}
