6c4e0f1f6d0d54bf245f191ea3bece87f3ed4d98
Christopher Brown <pyrrhicpachyderm@gmail.com>
2021-10-06 12:32:21 +1300

    Add post processing functions for examining the weight of equivalently grouped models.

--- r/brute-post-process.R
+++ r/brute-post-process.R
@@ -12,6 +12,19 @@ information_criterion_weights <- function(information_criterion) {
 	return(weights)
 }
 
+prob_perm_test <- function(selected_probs, all_probs, num_iter = 1000) {
+	#Tests whether the sum of selected_probs is less than the sum of a random sample of all_probs of the same length.
+	#Returns the p value.
+	#TODO: Check that this is a valid statistical test.
+	selected_sum <- sum(selected_probs)
+	resamples <- replicate(num_iter-1,
+		sum(sample(all_probs, length(selected_probs)))
+	)
+	resamples <- c(selected_sum, resamples)
+	p <- mean(resamples <= selected_sum)
+	return(p)
+}
+
 BruteData <- R6::R6Class("BruteData",
 	public = list(
 		num_species = NULL,
@@ -129,6 +142,17 @@ BruteData <- R6::R6Class("BruteData",
 		},
 		weighted_col_coclassification_matrix = function() {
 			return(private$annotate_matrix(private$get_weighted_coclassification_matrix(self$col_groupings)))
+		},
+		
+		equivalently_grouped_weight = function() {
+			return(sum(self$equivalently_grouped_statistics$aic_weight))
+		},
+		equivalently_grouped_expected_weight = function() {
+			return(nrow(self$equivalently_grouped_statistics) / nrow(self$statistics))
+		},
+		equivalently_grouped_weight_p = function() {
+			#p value of a permutation test
+			return(prob_perm_test(self$equivalently_grouped_statistics$aic_weight, self$statistics$aic_weight))
 		}
 	)
 )
