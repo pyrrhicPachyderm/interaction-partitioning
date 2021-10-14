2b1c53c047d659f0b4ba6e9449f3f9e381781e98
Christopher Brown <pyrrhicpachyderm@gmail.com>
2021-10-14 19:50:59 +1300

    Add the GroupingMove class.

--- src/lattice.cpp
+++ src/lattice.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include "lattice.hpp"
 
 void GroupingLattice::constructAllGroupings() {
@@ -48,6 +49,29 @@ void GroupingLattice::constructAdjacencyLists() {
 				merges[sourceIndex].push_back(destIndex);
 				splits[destIndex].push_back(sourceIndex);
 			}
+		}
+	}
+}
+
+GroupingMove::GroupingMove(std::vector<size_t> splitGrouping, std::vector<size_t> mergedGrouping, size_t g1, size_t g2) {
+	assert(splitGrouping.size() == mergedGrouping.size());
+	
+	splitGroups = std::make_pair(g1, g2);
+	
+	size_t numSplitGroups = *std::max_element(splitGrouping.begin(), splitGrouping.end()) + 1; //+1 for zero indexing.
+	size_t numMergedGroups = *std::max_element(mergedGrouping.begin(), mergedGrouping.end()) + 1; //+1 for zero indexing.
+	
+	assert(numSplitGroups == numMergedGroups+1);
+	
+	mergeMap = std::vector<size_t>(numSplitGroups);
+	splitMap = std::vector<size_t>(numMergedGroups);
+	
+	for(size_t i = 0; i < splitGrouping.size(); i++) {
+		if(splitGrouping[i] == splitGroups.first || splitGrouping[i] == splitGroups.second) {
+			mergedGroup = mergedGrouping[i];
+		} else {
+			mergeMap[splitGrouping[i]] = mergedGrouping[i];
+			splitMap[mergedGrouping[i]] = splitGrouping[i];
 		}
 	}
 }
--- src/lattice.hpp
+++ src/lattice.hpp
@@ -1,7 +1,10 @@
 #ifndef LATTICE_HPP
 #define LATTICE_HPP
 
+#include <assert.h>
 #include "grouping.hpp"
+
+class GroupingMove;
 
 //A GroupingLattice is the set of all partitions of a given number of species, partially ordered by refinement.
 //It forms a lattice, in the mathematical sense that there is a unique supremum and a unique infimum.
@@ -25,6 +28,32 @@ class GroupingLattice {
 			constructAllGroupings();
 			constructAdjacencyLists();
 		};
+};
+
+//A move between two partitions, by merging two groups or splitting one.
+//The same move represents both directions.
+class GroupingMove {
+	protected:
+		size_t mergedGroup;
+		std::pair<size_t,size_t> splitGroups;
+		
+		//The maps are group-to-group mappings for the groups that stay the same.
+		//Index into them using groups from the source grouping, and they return groups from the destination grouping.
+		std::vector<size_t> mergeMap;
+		std::vector<size_t> splitMap;
+	public:
+		//g1 and g2 are the two group numbers that are merged.
+		GroupingMove(std::vector<size_t> splitGrouping, std::vector<size_t> mergedGrouping, size_t g1, size_t g2);
+		
+		size_t getMergeMapping(size_t group) {
+			assert(group != splitGroups.first && group != splitGroups.second);
+			return mergeMap[group];
+		}
+		
+		size_t getSplitMapping(size_t group) {
+			assert(group != mergedGroup);
+			return splitMap[group];
+		}
 };
 
 #endif
