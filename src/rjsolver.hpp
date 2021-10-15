4d0ab1722c7335898e716350f7e5dd7ec987da8a
Christopher Brown <pyrrhicpachyderm@gmail.com>
2021-10-15 20:52:34 +1300

    Add a Boolean to ReversibleJumpSolver to see whether each type of grouping can be changed.

--- src/rjsolver.hpp
+++ src/rjsolver.hpp
@@ -11,6 +11,7 @@ class ReversibleJumpSolver : public Solver {
 		static double aicHyperprior(GroupingSet groupings);
 	protected:
 		typedef std::array<size_t, NUM_GROUPING_TYPES> GroupingIndexSet;
+		typedef std::array<bool, NUM_GROUPING_TYPES> GroupingBooleanSet;
 		
 		GroupingLattice groupingLattice;
 		HyperpriorFunc hyperpriorFunc;
@@ -18,16 +19,18 @@ class ReversibleJumpSolver : public Solver {
 		GroupingIndexSet currentGroupings;
 		Parameters currentParameters;
 		
+		GroupingBooleanSet isChangingGroupings;
+		
 		//If isProposing, then base class functions such as getResiduals() will use the propsed groupings.
 		bool isProposing = false;
 		
 		GroupingIndexSet proposedGroupings;
 		Parameters proposedParameters;
 	public:
-		ReversibleJumpSolver(Data data, HyperpriorFunc hyperpriorFunc):
-			Solver(data), groupingLattice(GroupingLattice(data.numSpecies)), hyperpriorFunc(hyperpriorFunc) {};
-		ReversibleJumpSolver(Data data):
-			ReversibleJumpSolver(data, aicHyperprior) {};
+		ReversibleJumpSolver(Data data, HyperpriorFunc hyperpriorFunc, GroupingBooleanSet isChangingGroupings):
+			Solver(data), groupingLattice(GroupingLattice(data.numSpecies)), hyperpriorFunc(hyperpriorFunc), isChangingGroupings(isChangingGroupings) {};
+		ReversibleJumpSolver(Data data, GroupingBooleanSet isChangingGroupings):
+			ReversibleJumpSolver(data, aicHyperprior, isChangingGroupings) {};
 	protected:
 		//Functions to say that particular elements have changed and mark appropriate things as dirty.
 		void dirtyDataSubclass() override {}
