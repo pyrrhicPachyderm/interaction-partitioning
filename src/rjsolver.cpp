d6ed0ab3577e29cffaa4c12a32d5c5d591bc2f42
Christopher Brown <pyrrhicpachyderm@gmail.com>
2021-10-15 15:16:56 +1300

    Add a member function to ReversibleJumpSolver to convert grouping indices to groupings.

--- src/rjsolver.cpp
+++ src/rjsolver.cpp
@@ -4,3 +4,9 @@ double aicHyperprior(GroupingSet groupings) {
 	size_t numParameters = groupings[GROWTH].getNumGroups() + groupings[ROW].getNumGroups() * groupings[COL].getNumGroups();
 	return exp(-numParameters);
 }
+
+GroupingSet ReversibleJumpSolver::getGroupings(GroupingIndexSet groupingIndices) const {
+	//TODO: This should be done based on NUM_GROUPING_TYPES, but constructing a std::array is hard.
+	//Perhaps look at https://stackoverflow.com/a/32175958
+	return GroupingSet({groupingLattice.getGrouping(groupingIndices[GROWTH]), groupingLattice.getGrouping(groupingIndices[ROW]), groupingLattice.getGrouping(groupingIndices[COL])});
+}
