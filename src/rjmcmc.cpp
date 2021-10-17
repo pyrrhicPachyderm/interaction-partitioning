677f29c05eb6ab6a7e8d5540b158b2f15859c94a
Christopher Brown <pyrrhicpachyderm@gmail.com>
2021-10-17 15:27:01 +1300

    Make the rjmcmc program run a full burn-in and chain, with output.
    It only outputs the grouings for now, but that's also all we care about for now.

--- src/rjmcmc.cpp
+++ src/rjmcmc.cpp
@@ -3,6 +3,9 @@
 #include "input.hpp"
 #include "rjsolver.hpp"
 
+#define BURN_IN 1e5
+#define NUM_STEPS 1e5
+
 int main(int argc, char **argv) {
 	Input input = readInput(argc, argv);
 	
@@ -10,6 +13,18 @@ int main(int argc, char **argv) {
 	grouping.separate();
 	
 	ReversibleJumpSolver solver(input.getData(), {grouping, grouping, grouping}, {false, true, true});
+	
+	solver.burnIn(BURN_IN);
+	
+	OutputColumn<Grouping> outputRowGroupings("row_group");
+	OutputColumn<Grouping> outputColGroupings("col_group");
+	for(size_t i = 0; i < NUM_STEPS; i++) {
+		solver.makeJump();
+		outputRowGroupings.insert(solver.getGrouping(ROW));
+		outputColGroupings.insert(solver.getGrouping(COL));
+	}
+	
+	outputTable(input.getOutputFile(), outputRowGroupings, outputColGroupings);
 	
 	return 0;
 }
