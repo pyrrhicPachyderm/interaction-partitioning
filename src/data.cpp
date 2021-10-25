c9f8b1902f77430f626f051061f29a3a65ac8ce3
Christopher Brown <pyrrhicpachyderm@gmail.com>
2021-10-26 12:36:49 +1300

    Correct an error in Data::guessCompetitionCoefficientMagnitude().
    1 - sum_j N_j alpha_j is equal to zero when \alpha_j is equal to 1 / sum_j N_j.
    I had an extra variable in there, due to confusion about the inclusion of N_i.
    But this whole term is multiplied by N_i separately, so N_i is irrelevant.

--- src/data.cpp
+++ src/data.cpp
@@ -20,6 +20,6 @@ double Data::guessGrowthRate() const {
 
 double Data::guessCompetitionCoefficientMagnitude() const {
 	//We will assume that with all species present at average density, growth halts.
-	//This gives us 1, divided by the square of average density, divided by the number of species.
-	return  1.0 / design.mean() / design.mean() / numSpecies;
+	//This gives us 1, divided by the average density, divided by the number of species.
+	return  1.0 / design.mean() / numSpecies;
 }
