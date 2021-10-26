5ff6e939de8d1a8f0c1d8d8d65af07c3d7d46f8f
Christopher Brown <pyrrhicpachyderm@gmail.com>
2021-10-26 21:20:23 +1300

    Add a post-processing script for reversible jump data.

---
+++ r/rjmcmc-post-process.R
@@ -0,0 +1,17 @@
+#A set of helper functions for post-processing the data from reversible jump MCMC.
+#Depends upon post-process.R
+
+library(magrittr)
+
+RJMCMCData <- R6::R6Class("RJMCMCData",
+	inherit = Data,
+	
+	active = list(
+		weighted_row_coclassification_matrix = function() {
+			return(private$annotate_matrix(private$get_average_coclassification_matrix(self$row_groupings)))
+		},
+		weighted_col_coclassification_matrix = function() {
+			return(private$annotate_matrix(private$get_average_coclassification_matrix(self$col_groupings)))
+		}
+	)
+)
