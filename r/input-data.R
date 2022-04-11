a2b88132502d869cbfbcdef31e51b1860def0d08
Christopher Brown <pyrrhicpachyderm@gmail.com>
2022-04-12 10:28:02 +1200

    Add an `is_per_capita` variable to the InputData object.

--- r/input-data.R
+++ r/input-data.R
@@ -7,13 +7,15 @@ InputData <- R6::R6Class("InputData",
 		focal_vector = NULL,
 		response_vector = NULL,
 		design_matrix = NULL,
+		is_per_capita = NULL,
 		num_species = NULL,
 		num_obs = NULL,
 		
-		initialize = function(focal_vector_file_name, response_vector_file_name, design_matrix_file_name) {
+		initialize = function(focal_vector_file_name, response_vector_file_name, design_matrix_file_name, is_per_capita=FALSE) {
 			self$focal_vector <- read.table(focal_vector_file_name)[[1]]
 			self$response_vector <- read.table(response_vector_file_name)[[1]]
 			self$design_matrix <- as.matrix(read.table(design_matrix_file_name))
+			self$is_per_capita <- is_per_capita
 			
 			self$num_species <- ncol(self$design_matrix)
 			self$num_obs <- nrow(self$design_matrix)
