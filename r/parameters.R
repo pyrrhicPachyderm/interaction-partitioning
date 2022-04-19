99e47a6f291ceda1e05ac88048f7f8e36b3035c2
Christopher Brown <pyrrhicpachyderm@gmail.com>
2022-04-19 12:51:50 +1200

    Add functions find the weighted and unweighted mean of sets of parameters.

--- r/parameters.R
+++ r/parameters.R
@@ -11,3 +11,17 @@ Parameters <- R6::R6Class("Parameters",
 		}
 	)
 )
+
+parameters_weighted_mean <- function(parameters_list, weights) {
+	growth_rates_list <- lapply(parameters_list, function(p){p$growth_rates})
+	alpha_values_list <- lapply(parameters_list, function(p){p$alpha_values})
+	weighted_growth_rates_list <- lapply(1:length(weights), function(i){growth_rates_list[[i]] * weights[i]})
+	weighted_alpha_values_list <- lapply(1:length(weights), function(i){alpha_values_list[[i]] * weights[i]})
+	growth_rates <- Reduce("+", weighted_growth_rates_list) / sum(weights)
+	alpha_values <- Reduce("+", weighted_alpha_values_list) / sum(weights)
+	return(Parameters$new(growth_rates, alpha_values))
+}
+
+parameters_mean <- function(parameters_list) {
+	parameters_weighted_mean(parameters_list, rep(1, length(parameters_list)))
+}
