e71d2689a4e826872c831b915f918500c42706ed
Christopher Brown <pyrrhicpachyderm@gmail.com>
2022-08-30 15:04:58 +1200

    Rewrite depend.sh to reduce code duplication in the case statement.

--- depend.sh
+++ depend.sh
@@ -5,11 +5,13 @@ cc="$1"
 shift 1
 dir="$1"
 shift 1
-case "$dir" in
-	"" | ".")
-		"$cc" -MM -MG "$@" | sed -e 's@^\(.*\)\.o:@.PRECIOUS \1.d \1.o:@'
-		;;
-	*)
-		"$cc" -MM -MG "$@" | sed -e "s@^\(.*\)\.o:@.PRECIOUS $dir/\1.d $dir/\1.o:@"
-		;;
-esac
+
+"$cc" -MM -MG "$@" |
+	case "$dir" in
+		"" | ".")
+			sed -e 's@^\(.*\)\.o:@.PRECIOUS \1.d \1.o:@'
+			;;
+		*)
+			sed -e "s@^\(.*\)\.o:@.PRECIOUS $dir/\1.d $dir/\1.o:@"
+			;;
+	esac
