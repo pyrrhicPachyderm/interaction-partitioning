296bd8e10777dd0043aa7f79551c3f9c4171b23b
Christopher Brown <pyrrhicpachyderm@gmail.com>
2022-09-23 13:20:14 +1200

    Add the Distribution module, for dealing wtih statistical distributions.

---
+++ src/distribution.cpp
@@ -0,0 +1,27 @@
+#include <math.h>
+#include <random>
+#include "distribution.hpp"
+
+#define RANDOM_SEED 42
+
+static std::default_random_engine randomNumberGenerator(RANDOM_SEED);
+
+double Distributions::Uniform::getDensity(double x) const {
+	if(x >= min && x < max) return 1.0 / (max - min);
+	else return 0;
+}
+
+double Distributions::Uniform::getRandom() const {
+	return std::uniform_real_distribution(min, max)(randomNumberGenerator);
+}
+
+double Distributions::Normal::getDensity(double x) const {
+	double residual = x - mean;
+	return 1.0 / sqrt(2 * M_PI * variance) * exp(-0.5 * residual*residual / variance);
+}
+
+double Distributions::Normal::getRandom() const {
+	return std::normal_distribution(mean, sqrt(variance))(randomNumberGenerator);
+}
+
+Distribution<double> a(new Distributions::Normal(0,1));
---
+++ src/distribution.hpp
@@ -0,0 +1,53 @@
+#ifndef DISTRIBUTION_HPP
+#define DISTRIBUTION_HPP
+
+#include <vector>
+#include <memory>
+
+namespace Distributions {
+	template<typename DomainValue> class Base {
+		public:
+			virtual double getDensity(DomainValue x) const = 0;
+			virtual DomainValue getRandom() const = 0;
+			
+			virtual ~Base() {};
+	};
+	
+	class Uniform : public Base<double> {
+		protected:
+			double min;
+			double max;
+		public:
+			Uniform(double min, double max):
+				min(min), max(max) {};
+			
+			double getDensity(double x) const override;
+			double getRandom() const override;
+	};
+	
+	class Normal : public Base<double> {
+		protected:
+			double mean;
+			double variance;
+		public:
+			Normal(double mean, double variance):
+				mean(mean), variance(variance) {};
+			
+			double getDensity(double x) const override;
+			double getRandom() const override;
+	};
+}
+
+//A wrapper class to deal with maintaining the pointer required for runtime polymorphism.
+template<typename DomainValue> class Distribution {
+	protected:
+		std::shared_ptr<const Distributions::Base<DomainValue>> d;
+	public:
+		Distribution(const Distributions::Base<DomainValue> *d):
+			d(d) {}
+		
+		double getDensity(DomainValue x) const {return d->getDensity(x);};
+		DomainValue getRandom() const {return d->getRandom();};
+};
+
+#endif
