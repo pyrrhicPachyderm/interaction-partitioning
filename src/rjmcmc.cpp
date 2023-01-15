f4ddb0b61dfe4c14c72d4707dc429b71c64f87df
Christopher Brown <pyrrhicpachyderm@gmail.com>
2023-01-15 17:16:11 +1300

    Make the RJMCMC program run ten chains.

--- src/rjmcmc.cpp
+++ src/rjmcmc.cpp
@@ -7,6 +7,7 @@
 #define NUM_DIALS 1e2
 #define BURN_IN 1e5
 #define NUM_STEPS 1e5
+#define NUM_CHAINS 10
 
 int main(int argc, char **argv) {
 	Input input(argc, argv, {'a'});
@@ -22,19 +23,22 @@ int main(int argc, char **argv) {
 	
 	ReversibleJumpSolver solver(input.getData(), hyperprior, {rowGrouping, rowGrouping, colGrouping}, {false, true, true});
 	
-	solver.dialIn(JUMPS_PER_DIAL, NUM_DIALS);
-	solver.burnIn(BURN_IN);
-	
 	OutputColumn<Grouping> outputRowGroupings("row_group");
 	OutputColumn<Grouping> outputColGroupings("col_group");
 	OutputColumn<Parameters> outputParameters("parameters");
 	OutputColumn<double> outputErrorVariance("parameters_error_variance");
-	for(size_t i = 0; i < NUM_STEPS; i++) {
-		solver.makeJump();
-		outputRowGroupings.insert(solver.getGrouping(ROW));
-		outputColGroupings.insert(solver.getGrouping(COL));
-		outputParameters.insert(solver.getParameters());
-		outputErrorVariance.insert(solver.getParameters().getAdditionalParameter(0));
+	
+	solver.dialIn(JUMPS_PER_DIAL, NUM_DIALS);
+	
+	for(size_t chain = 0; chain < NUM_CHAINS; chain++) {
+		solver.burnIn(BURN_IN);
+		for(size_t i = 0; i < NUM_STEPS; i++) {
+			solver.makeJump();
+			outputRowGroupings.insert(solver.getGrouping(ROW));
+			outputColGroupings.insert(solver.getGrouping(COL));
+			outputParameters.insert(solver.getParameters());
+			outputErrorVariance.insert(solver.getParameters().getAdditionalParameter(0));
+		}
 	}
 	
 	outputTable(input.getOutputFile(), outputRowGroupings, outputColGroupings, outputParameters, outputErrorVariance);
