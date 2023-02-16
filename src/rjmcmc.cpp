e3b418ffc964b37ddd89acb1f5b5dcaf080069a8
Christopher Brown <pyrrhicpachyderm@gmail.com>
2023-02-17 00:41:37 +1300

    Setup the rjmcmc main() function for parallelisation.
    Ensure that each chain runs independently, writing to no variables
    that any other chain could use simultaneously.

--- src/rjmcmc.cpp
+++ src/rjmcmc.cpp
@@ -10,6 +10,8 @@
 #define DEFAULT_NUM_CHAINS 10
 #define DEFAULT_THINNING_FACTOR 1
 
+#define RANDOM_SEED 42
+
 int main(int argc, char **argv) {
 	Input input(argc, argv, true,
 		{'a', 'g'},
@@ -36,30 +38,41 @@ int main(int argc, char **argv) {
 	size_t numChains = input.getIntOptResult('c');
 	size_t thinningFactor = input.getIntOptResult('t');
 	
+	size_t numOutputsPerChain = numSteps / thinningFactor;
+	size_t numOutputs = numOutputsPerChain * numChains;
+	
 	Model model = Model(new Models::LotkaVolterra());
-	ReversibleJumpSolver solver(model, input.getData(), hyperprior, parametersPrior, {rowGrouping, rowGrouping, colGrouping}, {isGrowthVarying, true, true});
+	ReversibleJumpSolver masterSolver(model, input.getData(), hyperprior, parametersPrior, {rowGrouping, rowGrouping, colGrouping}, {isGrowthVarying, true, true});
 	
-	OutputColumn<Grouping> outputGrowthGroupings("growth_group");
-	OutputColumn<Grouping> outputRowGroupings("row_group");
-	OutputColumn<Grouping> outputColGroupings("col_group");
-	OutputColumn<Parameters> outputParameters("parameters");
-	OutputColumn<double> outputErrorVariance("parameters_error_variance");
-	OutputColumn<size_t> outputChainID("chain_id");
+	//Only groupings need actual default values; the rest have default constructors.
+	OutputColumn<Grouping> outputGrowthGroupings("growth_group", numOutputs, masterSolver.getGrouping(GROWTH));
+	OutputColumn<Grouping> outputRowGroupings("row_group", numOutputs, masterSolver.getGrouping(ROW));
+	OutputColumn<Grouping> outputColGroupings("col_group", numOutputs, masterSolver.getGrouping(COL));
+	OutputColumn<Parameters> outputParameters("parameters", numOutputs, Parameters());
+	OutputColumn<double> outputErrorVariance("parameters_error_variance", numOutputs, 0.0);
+	OutputColumn<size_t> outputChainID("chain_id", numOutputs, 0);
 	
-	solver.dialIn(jumpsPerDial, numDials);
+	masterSolver.setSeed(RANDOM_SEED);
+	masterSolver.dialIn(jumpsPerDial, numDials);
+	masterSolver.resetChain();
 	
 	for(size_t chain = 0; chain < numChains; chain++) {
-		solver.resetChain();
+		ReversibleJumpSolver solver = masterSolver;
+		solver.setSeed(RANDOM_SEED + chain + 1); //+1 so that chain 0 doesn't use the same seed as dialing in.
 		solver.burnIn(burnIn);
+		
+		size_t outputIndex = numOutputsPerChain * chain;
 		for(size_t i = 0; i < numSteps; i++) {
 			solver.makeJump();
 			if(i%thinningFactor == 0) {
-				outputGrowthGroupings.insert(solver.getGrouping(GROWTH));
-				outputRowGroupings.insert(solver.getGrouping(ROW));
-				outputColGroupings.insert(solver.getGrouping(COL));
-				outputParameters.insert(Parameters(solver.getParameters(), solver.getGroupings()));
-				outputErrorVariance.insert(solver.getParameters().getAdditionalParameter(0));
-				outputChainID.insert(chain);
+				outputGrowthGroupings.set(outputIndex, solver.getGrouping(GROWTH));
+				outputRowGroupings.set(outputIndex, solver.getGrouping(ROW));
+				outputColGroupings.set(outputIndex, solver.getGrouping(COL));
+				outputParameters.set(outputIndex, Parameters(solver.getParameters(), solver.getGroupings()));
+				outputErrorVariance.set(outputIndex, solver.getParameters().getAdditionalParameter(0));
+				outputChainID.set(outputIndex, chain);
+				
+				outputIndex++;
 			}
 		}
 	}
