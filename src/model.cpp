b0acf9c6bb524ddd65aa16817cd2fa1423f9459b
Christopher Brown <pyrrhicpachyderm@gmail.com>
2023-02-19 17:22:56 +1300

    Add the Beverton-Holt model.

--- src/model.cpp
+++ src/model.cpp
@@ -19,3 +19,15 @@ double Models::LotkaVolterra::getGrowthRateJacobian(double focalDensity, double 
 double Models::LotkaVolterra::getCompetitionCoefficientJacobian(double focalDensity, double focalGrowthRate, const Eigen::VectorXd &densities, const Eigen::VectorXd &competitionCoefficients, size_t index) const {
 	return focalDensity * focalGrowthRate * (- densities[index]);
 }
+
+double Models::BevertonHolt::getResponse(double focalDensity, double focalGrowthRate, const Eigen::VectorXd &densities, const Eigen::VectorXd &competitionCoefficients) const {
+	return focalDensity * focalGrowthRate / (1.0 + densities.dot(competitionCoefficients));
+}
+
+double Models::BevertonHolt::getGrowthRateJacobian(double focalDensity, double focalGrowthRate, const Eigen::VectorXd &densities, const Eigen::VectorXd &competitionCoefficients) const {
+	return focalDensity / (1.0 + densities.dot(competitionCoefficients));
+}
+
+double Models::BevertonHolt::getCompetitionCoefficientJacobian(double focalDensity, double focalGrowthRate, const Eigen::VectorXd &densities, const Eigen::VectorXd &competitionCoefficients, size_t index) const {
+	return - focalDensity * focalGrowthRate * densities[index] / pow(1.0 + densities.dot(competitionCoefficients), 2);
+}
