dfc6896b01ebaf70ef38cbf166b0e479da37bbae
Christopher Brown <pyrrhicpachyderm@gmail.com>
2023-02-20 10:36:19 +1300

    Adjust the number of RJMCMC steps for the runs to achieve convergence.

--- Rules.mk
+++ Rules.mk
@@ -60,8 +60,8 @@ endef
 $(eval $(call output_template,test,indv,brute,brute,lotkavolterra,normal,))
 $(eval $(call output_template,tcl,indv,brute,brute,lotkavolterra,normal,))
 $(eval $(call output_template,tcl,pop,rjmcmc,rjmcmc,bevertonholt,negativebinomial,-g -s1000000 -t10))
-$(eval $(call output_template,cxr,indv,rjmcmc,rjmcmc,bevertonholt,negativebinomial,-g -d15 -s100000000 -t1000))
-$(eval $(call output_template,goldberg,indv,rjmcmc,rjmcmc,bevertonholt,gamma,-g -s1000000 -t10))
+$(eval $(call output_template,cxr,indv,rjmcmc,rjmcmc,bevertonholt,negativebinomial,-g -d15 -s50000000 -t500))
+$(eval $(call output_template,goldberg,indv,rjmcmc,rjmcmc,bevertonholt,gamma,-g -s10000000 -t100))
 $(eval $(call output_template,carrara,time,rjmcmc,rjmcmc,lotkavolterra,normal,-g -d15))
 
 define article_analysis_template =
