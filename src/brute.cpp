fb40300a9cf1e416f0582d8336daabd4356e74fe
Christopher Brown <pyrrhicpachyderm@gmail.com>
2024-10-11 19:54:12 +1000

    Make the brute force program allow non-normal error distributions.

--- src/brute.cpp
+++ src/brute.cpp
@@ -4,13 +4,22 @@
 int main(int argc, char **argv) {
 	Input input(argc, argv, false);
 	
-	if(input.getErrorDistribution() != "normal") {
-		fprintf(stderr, "Maximum likelihood solver does not support non-normal error distributions.\n");
+	std::string errorDistribution = input.getErrorDistribution();
+	std::string additionalParameterName;
+	
+	MaximumLikelihoodSolverInterface *solver = NULL;
+	if(errorDistribution == "normal") {
+		solver = new GaussNewtonSolver(input.getModel(), input.getData());
+	} else if(errorDistribution == "gamma") {
+		solver = new NLoptSolver<Distributions::Gamma2>(input.getModel(), input.getData());
+	} else if(errorDistribution == "negativebinomial") {
+		solver = new NLoptSolver<Distributions::DiscreteWrapper<Distributions::NegativeBinomial2>>(input.getModel(), input.getData());
+	} else {
+		fprintf(stderr, "Unrecognised error distribution.\n");
 		exit(1);
 	}
 	
-	GaussNewtonSolver solver = GaussNewtonSolver(input.getModel(), input.getData());
-	solver.updateGrouping(GROWTH, &Grouping::separate);
+	solver->updateGrouping(GROWTH, &Grouping::separate);
 	
 	OutputColumn<Grouping> outputRowGroupings("row_group");
 	OutputColumn<Grouping> outputColGroupings("col_group");
@@ -18,17 +27,18 @@ int main(int argc, char **argv) {
 	OutputColumn<double> outputAICcs("aicc");
 	OutputColumn<double> outputR2s("R2");
 	OutputColumn<Parameters> outputParameters("parameters");
+	//TODO: Output additional parameters for generalised models.
 	
 	do {
 		do {
-			outputRowGroupings.insert(solver.getGrouping(ROW));
-			outputColGroupings.insert(solver.getGrouping(COL));
-			outputAICs.insert(solver.getAIC());
-			outputAICcs.insert(solver.getAICc());
-			outputR2s.insert(solver.getR2());
-			outputParameters.insert(Parameters(solver.getSolutionParameters(), solver.getGroupings()));
-		} while(solver.updateGrouping(ROW, &Grouping::advance));
-	} while(solver.updateGrouping(COL, &Grouping::advance));
+			outputRowGroupings.insert(solver->getGrouping(ROW));
+			outputColGroupings.insert(solver->getGrouping(COL));
+			outputAICs.insert(solver->getAIC());
+			outputAICcs.insert(solver->getAICc());
+			outputR2s.insert(solver->getR2());
+			outputParameters.insert(Parameters(solver->getSolutionParameters(), solver->getGroupings()));
+		} while(solver->updateGrouping(ROW, &Grouping::advance));
+	} while(solver->updateGrouping(COL, &Grouping::advance));
 	
 	outputTable(input.getOutputFile(), outputRowGroupings, outputColGroupings, outputParameters, outputAICs, outputAICcs, outputR2s);
 	
